8df44bbe1576fb7b193734f494153012b1de577c
normalframe1094 <kapol2990@gmail.com>
2026-01-30 14:12:47 +0900

    주소추가/상품 이미지 링크 생성 -> 상세 페이지/상품 하나 당 여러 이미지 넣기 2

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+# get_object_or_404 >> 페이지 없음(404)
+from .models import Product
+from django.db.models import F
+from django.urls import reverse
+from django.views import generic
+from django.http import HttpResponseRedirect, JsonResponse
+from django.utils import timezone
+from django.urls import reverse_lazy
+import datetime
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = "shop/home.html"
+    context_object_name = "product_list"
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+class ProductDetailView(generic.DetailView):
+        model = Product
+        template_name = "shop/product_detail.html"
